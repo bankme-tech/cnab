08ae98f2ac43efa91557c27adb4590bcdcb813d2
lumiano <k_l.s@outlook.com>
2023-09-28 22:09:59 -0300

    feat: add class RegistroHeaderLabel for header cnab

---
+++ src/cnab/444/registro-header-label.ts
@@ -0,0 +1,169 @@
+import { BaseRegistroHeaderLabel } from '@/cnab/444/interfaces/base-registro-header-label.interface';
+import { BaseRegistroHeaderLayout } from '@/cnab/444/interfaces/base-registro-header-layout.interface';
+import { Decorators } from '@/decorators';
+import { plainToClass } from 'class-transformer';
+import {
+  IsAlphanumeric,
+  IsNotEmpty,
+  IsOptional,
+  Matches,
+  MaxLength,
+  validateSync,
+} from 'class-validator';
+
+/**
+ * Classe que representa o registro de rótulo do cabeçalho em um arquivo CNAB 444.
+ */
+export class RegistroHeaderLabel
+  implements BaseRegistroHeaderLabel, BaseRegistroHeaderLayout
+{
+  /**
+   * Identificação do registro (deve ser '0').
+   * @example '0'
+   */
+  @Decorators.PropertyPosition(1, 1, true, { allowLeadingZeros: false })
+  @MaxLength(1)
+  IdentificacaoRegistro: string = '0';
+
+  /**
+   * Identificação do arquivo de remessa (deve ser '1').
+   * @example '1'
+   */
+  @Decorators.PropertyPosition(2, 2, true, { allowLeadingZeros: false })
+  @MaxLength(1)
+  IdentificacaoArquivoRemessa: string = '1';
+
+  /**
+   * Literal de remessa (deve ser 'REMESSA').
+   * @example 'REMESSA'
+   */
+  @Decorators.PropertyPosition(3, 9, true, { allowLeadingZeros: false })
+  @MaxLength(7)
+  LiteralRemessa: string = 'REMESSA';
+
+  /**
+   * Código do serviço (deve ser '01').
+   * @example '01'
+   */
+  @Decorators.PropertyPosition(10, 11, true, { allowLeadingZeros: false })
+  @MaxLength(2)
+  CodigoServico: string = '01';
+
+  /**
+   * Literal de serviço (deve ser 'COBRANCA').
+   * @example 'COBRANCA'
+   */
+  @Decorators.PropertyPosition(12, 26, true, { allowLeadingZeros: false })
+  @MaxLength(15)
+  LiteralServico: string = 'COBRANCA';
+
+  /**
+   * Código do originador (máximo 20 caracteres alfanuméricos).
+   * @example '0123456789ABCDEF0123'
+   */
+  @Decorators.PropertyPosition(27, 46, true, {
+    allowLeadingZeros: true,
+    alphanumerics: true,
+  })
+  @MaxLength(20)
+  @IsNotEmpty()
+  CodigoOriginador: string = '';
+
+  /**
+   * Nome do originador (máximo 30 caracteres).
+   * @example 'NOME DO ORIGINADOR'
+   */
+  @Decorators.PropertyPosition(47, 76, true, { allowLeadingZeros: false })
+  @MaxLength(30)
+  @IsNotEmpty()
+  NomeOriginador: string = '';
+
+  /**
+   * Número do banco (deve ser '611').
+   * @example '611'
+   */
+  @Decorators.PropertyPosition(77, 79, true, { allowLeadingZeros: false })
+  @MaxLength(3)
+  NumeroBanco: string = '611';
+
+  /**
+   * Nome do banco (máximo 15 caracteres).
+   * @example 'NOME DO BANCO'
+   */
+  @Decorators.PropertyPosition(80, 94, true, { allowLeadingZeros: false })
+  @MaxLength(15)
+  @IsNotEmpty()
+  NomeBanco: string = 'PAULISTA S A';
+
+  /**
+   * Data de gravação do arquivo (formato 'DDMMAA').
+   * @example '250922'
+   */
+  @Decorators.PropertyPosition(95, 100, true, { allowLeadingZeros: false })
+  @Matches(/^([0-9]{6})$/)
+  @MaxLength(6)
+  DataGravacaoArquivo: string = '';
+
+  /**
+   * Campo em branco.
+   * @example ''
+   */
+  @Decorators.PropertyPosition(101, 108, true, { allowLeadingZeros: false })
+  @IsOptional()
+  Branco101: string = '';
+
+  /**
+   * Identificação do sistema (máximo 2 caracteres alfanuméricos).
+   * @example 'MX'
+   */
+  @Decorators.PropertyPosition(109, 110, true, { allowLeadingZeros: false })
+  @IsNotEmpty()
+  @MaxLength(2)
+  @IsAlphanumeric('pt-BR')
+  IdentificacaoSistema: string = 'MX';
+
+  /**
+   * Número sequencial do arquivo (máximo 7 dígitos).
+   * @example '0000001'
+   */
+  @Decorators.PropertyPosition(111, 117, true, { allowLeadingZeros: true })
+  @IsNotEmpty()
+  @MaxLength(7)
+  @Matches(/^([0-9]{7})$/)
+  NumeroSequencialArquivo: string = '';
+
+  /**
+   * Campo em branco.
+   * @example ''
+   */
+  @Decorators.PropertyPosition(118, 438, true, { allowLeadingZeros: false })
+  @IsOptional()
+  Branco118: string = '';
+
+  /**
+   * Número sequencial do registro (deve ser '000001').
+   * @example '000001'
+   */
+  @Decorators.PropertyPosition(439, 444, true, { allowLeadingZeros: true })
+  @IsNotEmpty()
+  @Matches(/^([0-9]{6})$/)
+  @MaxLength(6)
+  NumeroSequencialRegistro: string = '000001';
+
+  constructor(props: BaseRegistroHeaderLabel) {
+    Object.assign(this, props);
+  }
+
+  /**
+   * Valida os dados do registro.
+   * Lança um erro se os dados não forem válidos.
+   */
+  public validate(): void {
+    const instance = plainToClass(RegistroHeaderLabel, this);
+    const errors = validateSync(instance);
+
+    if (errors.length > 0) {
+      throw new Error(errors.toString());
+    }
+  }
+}
